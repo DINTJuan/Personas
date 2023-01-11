980767e596ec789bc0dff0a794f1c7662b601d2a
JuanMateoSanchez <90640548+JuanMateoSanchez@users.noreply.github.com>
2023-01-11 12:55:30 +0100

    Agregar archivos de proyecto.

---
+++ Servicios/PersonaServicio.cs
@@ -0,0 +1,22 @@
+﻿using Personas.Clases;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Personas.Servicios
+{
+    class PersonaServicio
+    {
+        public ObservableCollection<Persona> SacarPersonas()
+        {
+            ObservableCollection<Persona> personas = new ObservableCollection<Persona>();
+            personas.Add(new Persona("Pietro", 30, "Italiana"));
+            personas.Add(new Persona("Julia", 25, "Española"));
+            personas.Add(new Persona("Sophie", 35, "Francesa"));
+            return personas;
+        }
+    }
+}
